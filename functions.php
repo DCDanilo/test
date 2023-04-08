bb20de4acedfc8f8ce47b58fed9ec5d3578417a1
Danilo <danilo.decave@gmail.com>
2023-04-08 15:23:50 +0200

    header

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
 function themeStyle(){
     wp_enqueue_style('bootstrap', get_template_directory_uri().'/assets/css/bootstrap.min.css');
+    wp_enqueue_style('bootstrap', get_template_directory_uri().'/style.css');
     wp_enqueue_style('style', get_template_directory_uri().'/style.css');
+    
 }
 
 function themeScript(){
--- header.php
+++ header.php
@@ -5,10 +5,69 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
     <title>test</title>
 
     <?php wp_head(); ?>
 
 </head>
 <body <?php body_class()?> >
+
+
+<!-- top nav -->
+<div class="container-fluid top-nav ">
+    <div class="container justify-content-between py-2">
+        <div class="row px-4">
+            <div class="col">
+                <strong>sabato 8 aprile</strong>
+            </div>
+            <div class="col">
+                <div class="float-end">
+                    <i class="bi bi-facebook"></i>
+                    <i class="bi bi-instagram mx-2"></i>
+                    <i class="bi bi-twitter"></i>
+                </div>
+            </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+        </div>
+    </div>
+</div>
+<!-- end top nav -->
+
+<div class="container">
+    
+        <div>            
+            <a class="navbar-brand" href="/">
+                <img src="<?php echo get_template_directory_uri().'/assets/img/logo.png'?>" class="img-fluid" alt="">               
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </div>
+        
+
+    <nav class="nav navbar-expand-lg">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active border-end" aria-current="page" href="#">News</a>
+                </li>
+                <li class="nav-item border-end">
+                <a class="nav-link mx-2" href="#">Dove bere e mangiare</a>
+                </li>
+                <li class="nav-item border-end">
+                <a class="nav-link mx-2" href="#">Food & Drink</a>
+                </li>
+                <li class="nav-item border-end">
+                <a class="nav-link mx-2">Cosa fare in citta</a>
+                </li>
+            </ul>
+        </div>
+  </div>
+
+
+    </nav>
+    
+</div>
+
+
     
